2252c91a5c65774970aa675bbb2fe6301294aafd
makoto-0358 <kangdalingmu79@gmail.comgit config --global user.email kangdalingmu79@gmail.com>
2021-06-14 06:36:54 +0900

    first-commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const target = document.getElementsByClassName("header1-1-2--res")[0];
+target.addEventListener('click', () => {
+  target.classList.toggle('open');
+});
+const target2 = document.getElementsByClassName("header1-1-2--res2")[0];
+target.addEventListener('click', () => {
+  target2.classList.toggle('in');
+});
